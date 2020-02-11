f63c670487215d7abaa20d64259472877fa7a5ce
Sam4uk <sto.ukr.net@gmail.com>
2020-02-11 22:07:37 +0200

    Перша прогрограма на с++
    Файл завантажено через браузер

---
+++ hello.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+// Перша програма написана на курсі Сі++ ITEA
+/*
+ * Вчу наново C++
+ * Та освоюю Git
+ * 
+ * Для компіляції коритуватися консоллю команда
+ * Clang++ -Wall -Wpedantic -std=c++17 hello.cpp
+ * На виході отримаємо виконуваний файл a.out
+ * */
+int main()
+{
+	std::cout<<"Hello World!"<<std::endl;
+	return 0;
+}
